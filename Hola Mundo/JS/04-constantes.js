228fda1b772187bab58251dd335c3de21907e7fe
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-21 13:22:03 +0100

    constantes

---
+++ Hola Mundo/JS/04-constantes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Constantes
+var web = "http://llilles.es";
+const ip = "192.88.0.12";
+console.log(web, ip);
+
+web = "http://llillesTíbaliz.es";
+ip = "192.88.9.19"; //error consola
+console.log(web, ip);
