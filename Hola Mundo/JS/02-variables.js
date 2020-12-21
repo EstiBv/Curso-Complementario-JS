dc7dbaa44abf153d40d507d9413999f1436834f2
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-21 12:48:17 +0100

    variables

---
+++ Hola Mundo/JS/02-variables.js
@@ -0,0 +1,18 @@
+// VARIABLES
+
+/* var | let | const */
+var country = "Spain";
+var city = "Badajoz";
+var seniority = 2020;
+
+// esta var (como log y alert) muestra primeros valores o reasignados, segun ubicación
+var city_and_country = city + " " + country;
+
+// reasignar de valores
+country = "Mexico";
+city = "Veracruz";
+
+// concatenación
+
+console.log(country, city, seniority);
+alert(city_and_country);
