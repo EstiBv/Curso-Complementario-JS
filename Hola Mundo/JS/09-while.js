f994a2f5cb5a284731936f3ebbfc1517e161ad43
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-22 12:05:16 +0100

    while do

--- Hola Mundo/JS/09-while.js
+++ Hola Mundo/JS/09-while.js
@@ -1,9 +1,20 @@
 "use strict";
 
-// While
+// While (Si añadimos break, para de iterar)
 let year = 2018;
 while (year <= 2030) {
-  // añadir operador para que no sea infinito
+  // añadir operador para que no sea infinito y corte la ejecución del bucle
   year++;
   console.log(year);
 }
+
+// Do while
+let years = 6;
+do {
+  alert("Solo cuando sea mayor o igual a 6");
+  // si no igualamos la condición la repetición es infinita
+  years--;
+  console.log(years);
+} while (years >= 4);
+
+//
