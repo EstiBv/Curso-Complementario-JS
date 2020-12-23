7aea8a44eec945aaddd66bb8bc0bd5457b588a77
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-23 10:45:28 +0100

    improvement exercise 3

--- Hola Mundo/JS/13-ejercicio-3.js
+++ Hola Mundo/JS/13-ejercicio-3.js
@@ -5,9 +5,13 @@
 let number1 = parseInt(prompt("Introduce el primer número", 0));
 let number2 = parseInt(prompt("Introduce el segundo número", 0));
 
-document.write(
-  "<h2>Entre " + number1 + " y " + number2 + " hay éstos números:</h2>"
-);
-for (let i = number1; i <= number2; i++) {
-  document.write(i + " </br>");
+if (number2 <= number1) {
+  document.write("El primer número ha de ser menor que el segundo");
+} else {
+  document.write(
+    "<h2>Entre " + number1 + " y " + number2 + " hay éstos números:</h2>"
+  );
+  for (let i = number1; i <= number2; i++) {
+    document.write(i + " </br>");
+  }
 }
