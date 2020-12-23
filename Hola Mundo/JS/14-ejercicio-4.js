fb5b1df304e1fd2aa3d2321036f976e4b9db463c
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-23 11:14:17 +0100

    refactored exercise 4

--- Hola Mundo/JS/14-ejercicio-4.js
+++ Hola Mundo/JS/14-ejercicio-4.js
@@ -6,20 +6,29 @@ Mostrar todos los números impares que hay entre dos números introducidos por e
 let number1 = parseInt(prompt("Introduce el primer número", 0));
 let number2 = parseInt(prompt("Introduce el segundo número", 0));
 
-if (number2 <= number1) {
-  document.write("El primer número ha de ser menor que el segundo");
-} else {
-  document.write(
-    "<h2>Los numeros impares entre " +
-      number1 +
-      " y " +
-      number2 +
-      " son éstos:</h2>"
-  );
-  for (let i = number1; i <= number2; i++) {
-    let impar = i % 2 === 1;
-    if (impar === true) {
-      document.write(i + " </br>");
-    }
+// if (number2 <= number1) {
+//   document.write("El primer número ha de ser menor que el segundo");
+// } else {
+//   document.write(
+//     "<h2>Los numeros impares entre " +
+//       number1 +
+//       " y " +
+//       number2 +
+//       " son éstos:</h2>"
+//   );
+//   for (let i = number1; i <= number2; i++) {
+//     let impar = i % 2 === 1;
+//     if (impar === true) {
+//       document.write(i + " </br>");
+//     }
+//   }
+// }
+
+// Refactorizado
+
+while (number1 < number2) {
+  number1++;
+  if (number1 % 2 === 1) {
+    document.write(number1 + " </br>");
   }
 }
