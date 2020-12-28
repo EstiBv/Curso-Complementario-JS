e15db444aec40e83884ee22ffec5f8e3aaf0e96c
Estibaliz Barato <estibalizbarato@gmail.com>
2020-12-28 15:53:55 +0100

    Dom

--- Hola Mundo/JS/28-dom.js
+++ Hola Mundo/JS/28-dom.js
@@ -1,6 +1,6 @@
 "use strtict";
 
-// let box = document.getElementById("js-box");
+//DOM - Document Object Model
 
 let box = document.querySelector("#js-box");
 
@@ -8,8 +8,10 @@ function changeColor(color) {
   box.style.background = color;
 }
 
-box.innerHTML = "Que tal?";
+// CONSEGUIR ELEMENTOS CON ID CONCRETO
+// let box = document.getElementById("js-box");
 
+box.innerHTML = "Que tal?";
 // html
 box.className = "js-box";
 
@@ -18,4 +20,22 @@ box.style.background = "red";
 box.style.padding = "20px";
 box.style.color = "white";
 
-console.log(box);
+// CONSEGUIR ELEMENTOS POR SU ETIQUETA
+let allLabels = document.getElementsByTagName("div");
+let textDiv2 = allLabels[1].textContent;
+allLabels[1].innerHTML = "Otro texto para este div en concreto";
+// console.log(allLabels, textDiv2);
+
+for (const valueLabel in allLabels) {
+  if (typeof allLabels[valueLabel].textContent === "string") {
+    let paragrapah = document.createElement("p");
+    let text = document.createTextNode(allLabels[valueLabel].textContent);
+    paragrapah.append(text);
+    document.querySelector("#mainSection").appendChild(paragrapah);
+  }
+}
+
+// CONSEGUIR ELEMENTOS POR SU CLASE CSS
+const divFirst = document.getElementsByClassName("first");
+
+console.log(divFirst);
