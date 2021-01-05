6004864ca55a3025ef1d11f29d0665494432c718
Estibaliz Barato <estibalizbarato@gmail.com>
2021-01-05 17:15:20 +0100

    dates

---
+++ Hola Mundo/JS/38-fechas.js
@@ -0,0 +1,19 @@
+"use strict";
+// fecha y hora
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth();
+var day = date.getDate();
+var hour = date.getHours();
+var minutes = date.getMinutes();
+console.log(date);
+
+var textHour =
+  ` El año es: ${year}` +
+  ` El mes es ${month}` +
+  ` El día es: ${day}` +
+  ` y son las ${hour}` +
+  `:` +
+  `${minutes}`;
+console.log(textHour);
